4b1788398fb522872f36de8f910f1538717faca2
Sayco-ahmed <59507720+Sayco-ahmed@users.noreply.github.com>
2020-09-23 00:54:02 +0200

    semi-final

--- js/main.js
+++ js/main.js
@@ -37,6 +37,71 @@
 	};
 	loader();
 
+
+
+	/*------------------
+		Hero Slider
+	--------------------*/
+	var sliderCount;
+	$(".hero-slider").on("initialized.owl.carousel", function(e) {
+		sliderCount = e.item.count;
+		if( sliderCount < 10) {
+			sliderCount = "0" + sliderCount;
+		}
+	}).owlCarousel({
+		autoplayHoverPause:true,
+        loop: true,
+        margin: 0,
+        nav: true,
+        items: 1,
+        dots: false,
+        animateOut: 'fadeOut',
+    	animateIn: 'fadeIn',
+		navText: ['<img src="img/icons/arrow-left-white.png" alt="">', '<img src="img/icons/arrow-right-black.png" alt="">'],
+        smartSpeed: 1200,
+		autoplay: true,
+		mouseDrag: false
+    }).on("changed.owl.carousel", function(e) {
+		
+		var Index = e.item.index - 1;
+		var Count = e.item.count;
+		var PreIndex = Index - 1;
+		var NextIndex = Index + 1;
+
+
+		if(PreIndex < 0) {
+			PreIndex = Count - 1;
+		}
+
+		if(PreIndex < 1) {
+			PreIndex = Count;
+		}
+
+		if (PreIndex < 10) {
+			PreIndex = "0" + PreIndex;
+		}
+
+		if(NextIndex > Count) {
+			NextIndex = 1;
+		}
+
+		if (NextIndex < 10) {
+			NextIndex = "0" + NextIndex;
+		}
+
+
+		$(".hero-slider .owl-nav button.owl-prev").html('<img src="img/icons/arrow-left-white.png" alt=""> <span> '+ PreIndex +'</span> ');
+		$(".hero-slider .owl-nav button.owl-next").html('<span> '+ NextIndex +' </span> <img src="img/icons/arrow-right-black.png" alt="">');
+	});
+
+	$(".hero-slider .owl-nav button.owl-prev").html('<img src="img/icons/arrow-left-white.png" alt=""> <span> '+ sliderCount +'</span> ');
+
+	$(".hero-slider .owl-nav button.owl-next").html('<span>02</span> <img src="img/icons/arrow-right-black.png" alt="">');
+
+
+
+
+
 	// Scrollax
    $.Scrollax();
 
@@ -216,6 +281,8 @@
 	};
 	contentWayPoint();
 
+
+	
 
 	// magnific popup
 	$('.image-popup').magnificPopup({
